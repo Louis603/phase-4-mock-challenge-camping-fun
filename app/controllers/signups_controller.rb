3d6ca62ae696fc6a5bedf4dff8008bf153d09650
Louis Y <louisy603@hotmail.com>
2022-06-14 11:46:59 -0400

    couldnt filter from /camper

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,19 @@
 class SignupsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable
+
+    def create
+        # singup = Singup.create(camper_id: params[:camper_id], activity_id: params[:activity_id], time: params:[:time])
+        signup = Signup.create!(signup_params)
+        render json: signup, includes: ['activity'], status: :created
+    end
+
+    private
+
+    def signup_params
+        params.permit(:camper_id, :activity_id, :time)
+    end
+
+    def unprocessable
+        render json: { error: "validation errors" }, status: 422
+    end
 end
--- app/serializers/camper_serializer.rb
+++ app/serializers/camper_serializer.rb
@@ -1,3 +1,10 @@
 class CamperSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :created_at, :updated_at
+
+  # has_many :signups
+  has_many :activities
+
+  # def activities_id
+  #     object.activities
+  # end
 end
